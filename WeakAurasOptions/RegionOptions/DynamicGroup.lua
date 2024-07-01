ff9828bd1c224d44b1068be6008f0d2caf22150e
NoM0Re <1629787+NoM0Re@users.noreply.github.com>
2024-07-01 22:23:06 +0200

    add missing dynamic group handling for non awesome_wotlk user

--- WeakAurasOptions/RegionOptions/DynamicGroup.lua
+++ WeakAurasOptions/RegionOptions/DynamicGroup.lua
@@ -142,11 +142,16 @@ local function createOptions(id, data)
       width = WeakAuras.normalWidth,
       name = L["Group by Frame"],
       order = 1.6,
-      values = {
-        ["UNITFRAME"] = L["Unit Frames"],
-        ["NAMEPLATE"] = L["Nameplates"],
-        ["CUSTOM"] = L["Custom Frames"],
-      },
+      values = function()
+        local v = {
+          ["UNITFRAME"] = L["Unit Frames"],
+          ["CUSTOM"] = L["Custom Frames"]
+        }
+        if WeakAuras.isAwesomeEnabled then
+          v["NAMEPLATE"] = L["Nameplates"]
+        end
+        return v
+      end,
       hidden = function() return data.grow == "CUSTOM" end,
       disabled = function() return not data.useAnchorPerUnit end
     },
