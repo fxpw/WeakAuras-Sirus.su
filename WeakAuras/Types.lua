d3815f4450a9770d38ee78d0c480eac2e476003e
NoM0Re <1629787+NoM0Re@users.noreply.github.com>
2026-07-30 04:52:55 +0200

    Add Font Flag Slug and Smooth Scaling
    The Slug Outline mode is incompatible with the drop shadows settings, so those get hidden.
    
    This also temporarily doesn't work on Titan/Classic, though Classic should have that working in a few days and Titan probably soonish too.

--- WeakAuras/Types.lua
+++ WeakAuras/Types.lua
@@ -1464,8 +1464,10 @@ Private.tick_placement_modes = {
 ---@type table<string, string>
 Private.font_flags = {
   None = L["None"],
+  ["SLUG"] = Private.SetOptionTextDisabled(WeakAuras.newFeatureString .. L["Slug"]),
   MONOCHROME = L["Monochrome"],
   OUTLINE = L["Outline"],
+  ["OUTLINE|SLUG"] = Private.SetOptionTextDisabled(WeakAuras.newFeatureString .. L["Slug Outline"]),
   THICKOUTLINE  = L["Thick Outline"],
   ["MONOCHROME|OUTLINE"] = L["Monochrome Outline"],
   ["MONOCHROME|THICKOUTLINE"] = L["Monochrome Thick Outline"]
